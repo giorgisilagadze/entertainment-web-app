1cac5206da8533d25dca280ec298c658871d03d0
giorgisilagadze <silagadze.gio123@gmail.com>
2023-07-26 22:37:47 +0400

    create global styles

---
+++ styles/Global.Styled.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+    * {
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+        font-family: 'Outfit', sans-serif;
+        background-color: #10141E;
+    }
+`;
