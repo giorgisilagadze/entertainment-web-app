75fac053d013122ee5b1b82b8f1244dda3ed3780
giorgisilagadze <silagadze.gio123@gmail.com>
2023-07-31 22:53:42 +0400

    mobile header is done

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,7 +1,58 @@
+import {
+  StyledHeader,
+  Category,
+  Avatar,
+  Logo,
+  ChildCategory,
+  StyledLinkCat,
+} from "@/styles/Header.Styled";
+import { useState } from "react";
+
+const category = [
+  {
+    source: "./assets/icon-nav-home.svg",
+    alt: "home",
+    link: "/",
+  },
+  {
+    source: "./assets/icon-nav-movies.svg",
+    alt: "movie",
+    link: "/movies",
+  },
+  {
+    source: "./assets/icon-nav-tv-series.svg",
+    alt: "tv-serie",
+    link: "/tv-series",
+  },
+  {
+    source: "./assets/icon-nav-bookmark.svg",
+    alt: "bookmark",
+    link: "/bookmark",
+  },
+];
+
 export default function Header() {
+  const [isChecked, setIsChecked] = useState("home");
   return (
-    <>
-      <h1>this is header</h1>
-    </>
+    <StyledHeader>
+      <Logo src="./assets/logo.svg" alt="logo" />
+      <Category>
+        {category.map((item) => (
+          <StyledLinkCat href={item.link} key={Math.random()}>
+            <ChildCategory
+              src={item.source}
+              alt={item.alt}
+              onClick={() => setIsChecked(item.alt)}
+              filter={
+                isChecked == item.alt
+                  ? "invert(100%) sepia(6%) saturate(0%) hue-rotate(115deg) brightness(508%) contrast(108%);"
+                  : ""
+              }
+            />
+          </StyledLinkCat>
+        ))}
+      </Category>
+      <Avatar src="./assets/image-avatar.png" alt="avatar" />
+    </StyledHeader>
   );
 }
---
+++ styles/Header.Styled.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { StyledLink } from "./LogIn.Styled";
+
+interface Style {
+  filter?: string;
+}
+
+export const StyledHeader = styled.div`
+  width: 100%;
+  padding: 16px;
+  background-color: #161d2f;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Category = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 30px;
+`;
+
+export const Avatar = styled.img`
+  width: 24px;
+  height: 24px;
+  border-radius: 24px;
+  border: 1px solid var(--pure-white, #fff);
+`;
+
+export const Logo = styled.img`
+  width: 25px;
+  height: 20px;
+`;
+
+export const ChildCategory = styled(Logo)<Style>`
+  width: 16px;
+  height: 16px;
+  filter: ${({ filter }) => filter};
+`;
+
+export const StyledLinkCat = styled(StyledLink)`
+  width: 16px;
+  height: 16px;
+`;
